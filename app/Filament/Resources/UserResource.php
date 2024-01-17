afd97ac43949285e64ecd5269a5bbadba0fe745b
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-01-17 14:56:34 +0800

    modified user creation

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use App\Models\User;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Grid;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\UserResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\UserResource\RelationManagers;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+    protected static ?string $navigationGroup = 'Manage Users';
+    protected static ?string $navigationLabel  = 'Cashiers';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Grid::make()
+                    ->schema([
+                    Forms\Components\TextInput::make('name')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('email')
+                        ->email()
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('password')
+                        ->password()
+                        ->required()
+                        ->confirmed()
+                        ->maxLength(255)
+                        ->hidden(fn (string $operation): bool => $operation === 'edit'),
+                    Forms\Components\TextInput::make('password_confirmation')
+                            ->label('Confirm Password')
+                            ->password()
+                            ->required()
+                            ->maxLength(255)
+                            ->hidden(fn (string $operation): bool => $operation === 'edit'),
+                    Forms\Components\Select::make('role')
+                    ->options([
+                        'cashier' => 'Cashier',
+                    ])
+                    ->hidden()
+                        ->required()
+                        ->default('cashier')
+                        ->selectablePlaceholder(false)
+                    ])->columns(1)
+
+            ]);
+    }
+
+
+
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('role', 'cashier'))
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('role')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
